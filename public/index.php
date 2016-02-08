353981881674eec6d0fde899c625dd13623380bc
Joe Alamo <joe.keilty+github@gmail.com>
2016-02-08 17:49:46 +0000

    Only put app into debug mode based on environment variable

--- public/index.php
+++ public/index.php
@@ -9,7 +9,7 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $app = new Silex\Application();
-$app['debug'] = true;
+getenv('APP_ENV') === 'dev' ? $app['debug'] = true : $app['debug'] = false;
 
 
 $app->run();
