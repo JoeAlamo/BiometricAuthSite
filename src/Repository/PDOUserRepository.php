1634713fb10894bab62f3b1a728629a1ee8b9184
Joe Alamo <joe.keilty+github@gmail.com>
2016-02-09 19:16:48 +0000

    Fix UserRepository find and findByUsername methods

--- src/Repository/PDOUserRepository.php
+++ src/Repository/PDOUserRepository.php
@@ -16,40 +16,28 @@ class PDOUserRepository implements UserRepositoryInterface {
         $this->database = $database;
     }
 
-    /**
-     * @param int $id
-     * @return \BiometricSite\Model\User|false
-     */
-    public function find($id) {
-        return $this->findBy('user_id', $id, 1, \PDO::PARAM_INT);
-    }
-
-    /**
-     * @param string $username
-     * @return \BiometricSite\Model\User|false
-     */
-    public function findByUsername($username)
-    {
-        return $this->findBy('username', $username, 1);
-    }
-
-    /**
-     * @param string $column Name of column to restrict find
-     * @param mixed $value Value column must be
-     * @param int $limit Maximum results to return
-     * @param int $valueType The type of $value
-     * @return \BiometricSite\Model\User|false
-     */
-    private function findBy($column, $value, $limit, $valueType = \PDO::PARAM_STR) {
-        $stmt = $this->database->prepare("
+    public function find($user_id) {
+        $stmt = $this->database->prepare('
             SELECT *
             FROM user
-            WHERE :column = :value
-            LIMIT :limit
-        ");
-        $stmt->bindParam(':column', $column);
-        $stmt->bindParam(':value', $value, $valueType);
-        $stmt->bindParam(':limit', $limit, \PDO::PARAM_INT);
+            WHERE user_id = :id
+        ');
+        $stmt->bindParam(':id', $user_id);
+        $stmt->execute();
+
+        $stmt->setFetchMode(\PDO::FETCH_CLASS, 'BiometricSite\\Model\\User');
+
+        return $stmt->fetch();
+    }
+
+    public function findByUsername($username)
+    {
+        $stmt = $this->database->prepare('
+            SELECT *
+            FROM user
+            WHERE username = :username
+        ');
+        $stmt->bindParam(':username', $username);
         $stmt->execute();
 
         $stmt->setFetchMode(\PDO::FETCH_CLASS, 'BiometricSite\\Model\\User');
