94b3995d3f4a185f8964e6704010a438c4203a1e
Joe Alamo <joe.keilty+github@gmail.com>
2016-02-10 17:57:00 +0000

    Alter naming convention for application registrations

--- public/index.php
+++ public/index.php
@@ -15,7 +15,7 @@ getenv('APP_ENV') === 'dev' ? $app['debug'] = true : $app['debug'] = false;
  * PROVIDERS
  ********************************************************************************/
 $app->register(new Silex\Provider\ServiceControllerServiceProvider());
-$app->register(new \Silex\Provider\TwigServiceProvider(), [
+$app->register(new Silex\Provider\TwigServiceProvider(), [
     'twig.path' => __DIR__ . '/../resource/view',
 ]);
 $app->register(new BiometricSite\ServiceProvider\DatabaseServiceProvider());
@@ -24,24 +24,28 @@ $app->register(new BiometricSite\ServiceProvider\DatabaseServiceProvider());
  * REPOSITORIES
  ********************************************************************************/
 $app['repository.user'] = $app->share(function () use ($app) {
-   return new BiometricSite\Repository\PDOUserRepository($app['database']);
+    return new BiometricSite\Repository\PDOUserRepository($app['database']);
+});
 });
 
 /*********************************************************************************
  * SERVICES
  ********************************************************************************/
 $app['service.loginAuth'] = $app->share(function () use ($app) {
-   return new BiometricSite\Service\LoginAuthService($app['repository.user']);
+    return new BiometricSite\Service\LoginAuthService($app['repository.user']);
+});
 });
 
 /*********************************************************************************
  * CONTROLLERS
  ********************************************************************************/
-$app['home.controller'] = $app->share(function () use ($app) {
-   return new BiometricSite\Controller\HomeController($app['twig']);
+$app['controller.home'] = $app->share(function () use ($app) {
+    return new BiometricSite\Controller\HomeController($app['twig']);
 });
-$app['login.controller'] = $app->share(function () use ($app) {
-   return new BiometricSite\Controller\LoginAuthController($app['request_stack']->getCurrentRequest(), $app['twig'], $app['service.loginAuth']);
+
+$app['controller.loginAuth'] = $app->share(function () use ($app) {
+    return new BiometricSite\Controller\LoginAuthController($app['request_stack']->getCurrentRequest(), $app['twig'], $app['service.loginAuth']);
+});
 });
 
 /*********************************************************************************
@@ -49,7 +53,7 @@ $app['login.controller'] = $app->share(function () use ($app) {
  ********************************************************************************/
 $app->get('/', 'home.controller:indexAction');
 
-$app->get('/authentication/login', 'login.controller:indexAction');
-$app->post('/authentication/login', 'login.controller:loginAction');
+$app->get('/authentication/login', 'controller.loginAuth:indexAction');
+$app->post('/authentication/login', 'controller.loginAuth:loginAction');
 
 $app->run();
