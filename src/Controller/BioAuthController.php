bcf09f8d6cf7ed48357ef74cfb9c4afb665723c5
Joe Alamo <joe.keilty+github@gmail.com>
2016-02-10 18:01:03 +0000

    Add BioAuth controller for protocol
    Unsure as of yet whether there should be a controller for each version
    of the protocol. Leaning towards it. This means we can have an interface
    for each controller(/endpoint) which the individual bio auth services
    can rely upon.

---
+++ src/Controller/BioAuthController.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Joe Alamo
+ * Date: 09/02/2016
+ * Time: 12:10
+ */
+
+namespace BiometricSite\Controller;
+
+
+use BiometricSite\Service\BioAuth\V1\BioAuthServiceInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class BioAuthController {
+    private $request;
+    private $bioAuthService;
+
+    public function __construct(Request $request, BioAuthServiceInterface $bioAuthService) {
+        $this->request = $request;
+        $this->bioAuthService = $bioAuthService;
+    }
+
+    public function authenticateV1Action() {
+        return $this->bioAuthService->authenticate($this->request->request->get('client_id'), $this);
+    }
+
+    public function unknownClientId() {
+        return new Response('', Response::HTTP_FORBIDDEN);
+    }
+
+    public function invalidRequest() {
+        return new Response('', Response::HTTP_BAD_REQUEST);
+    }
+
+    public function bioAuthSuccessful($duration) {
+        $responseData = [
+            'expires' => $duration
+        ];
+
+        return new JsonResponse((object)$responseData, Response::HTTP_OK);
+    }
+} 
