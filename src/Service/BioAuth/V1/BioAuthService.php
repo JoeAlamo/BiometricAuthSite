ca38c8cfe16044b0c96b2a46a1221030bd379a00
Joe Alamo <joe.keilty+github@gmail.com>
2016-02-10 19:31:33 +0000

    Make BioAuthService create BioAuthSession

--- src/Service/BioAuth/V1/BioAuthService.php
+++ src/Service/BioAuth/V1/BioAuthService.php
@@ -9,13 +9,23 @@
 namespace BiometricSite\Service\BioAuth\V1;
 
 
+use BiometricSite\Repository\BioAuthSessionRepositoryInterface;
 use BiometricSite\Repository\BioClientRepositoryInterface;
+use BiometricSite\Repository\BioSessionRepositoryInterface;
 
 class BioAuthService implements BioAuthServiceInterface {
     private $bioClientRepository;
+    private $bioSessionRepository;
+    private $bioAuthSessionRepository;
 
-    public function __construct(BioClientRepositoryInterface $bioClientRepository) {
+    public function __construct(
+        BioClientRepositoryInterface $bioClientRepository,
+        BioSessionRepositoryInterface $bioSessionRepository,
+        BioAuthSessionRepositoryInterface $bioAuthSessionRepository
+    ) {
         $this->bioClientRepository = $bioClientRepository;
+        $this->bioSessionRepository = $bioSessionRepository;
+        $this->bioAuthSessionRepository = $bioAuthSessionRepository;
     }
 
     public function authenticate($client_id, $endpoint)
@@ -30,6 +40,9 @@ class BioAuthService implements BioAuthServiceInterface {
             return $endpoint->unknownClientId();
         }
         // Create biometric authenticated session for the client
+        $biometricSession = $this->bioSessionRepository->add(openssl_random_pseudo_bytes(24), null, null);
+        $biometricAuthenticatedSession = $this->bioAuthSessionRepository->add($client->biometric_client_id, $biometricSession->biometric_session_id, 30);
 
+        return $endpoint->bioAuthSuccessful($biometricAuthenticatedSession->expires);
     }
 }
