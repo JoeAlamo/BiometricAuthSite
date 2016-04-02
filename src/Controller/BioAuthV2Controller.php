cc9eeb86ad7f07dc226b08e15edcb957a72bdc99
Joe Alamo <joe.keilty+github@gmail.com>
2016-04-02 14:25:33 +0100

    Add skeleton V2 Controller & Service

---
+++ src/Controller/BioAuthV2Controller.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Joe Alamo
+ * Date: 09/02/2016
+ * Time: 12:10
+ */
+
+namespace BiometricSite\Controller;
+
+
+use BiometricSite\Service\BioAuthV2ServiceInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class BioAuthV2Controller implements BioAuthV2ControllerInterface {
+    private $request;
+    private $bioAuthService;
+
+    public function __construct(Request $request, BioAuthV2ServiceInterface $bioAuthService) {
+        $this->request = $request;
+        $this->bioAuthService = $bioAuthService;
+    }
+
+    public function stage1Action()
+    {
+        // TODO: Implement stage1Action() method.
+    }
+
+    public function stage1SuccessResponse($session_id, $server_id)
+    {
+        $responseData = [
+            'session_id' => $session_id,
+            'server_id' => $server_id
+        ];
+
+        return new JsonResponse((object)$responseData, Response::HTTP_CREATED);
+    }
+
+    public function stage2Action($session_id)
+    {
+        // TODO: Implement stage2Action() method.
+    }
+
+    public function stage2SuccessResponse($server_mac, $duration)
+    {
+        // TODO: Implement stage2SuccessResponse() method.
+    }
+
+    public function invalidClientIdResponse() {
+        return new Response('', Response::HTTP_FORBIDDEN);
+    }
+
+    public function invalidRequestResponse() {
+        return new Response('', Response::HTTP_BAD_REQUEST);
+    }
+
+    public function invalidClientMACResponse()
+    {
+        return new Response('', Response::HTTP_FORBIDDEN);
+    }
+
+    public function invalidSessionIdResponse()
+    {
+        return new Response('', Response::HTTP_NOT_FOUND);
+    }
+}
---
+++ src/Service/BioAuthV2Service.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Joe Alamo
+ * Date: 10/02/2016
+ * Time: 17:25
+ */
+
+namespace BiometricSite\Service;
+
+
+use BiometricSite\Controller\BioAuthV2ControllerInterface;
+use BiometricSite\Repository\BioAuthSessionRepositoryInterface;
+use BiometricSite\Repository\BioClientRepositoryInterface;
+use BiometricSite\Repository\BioSessionRepositoryInterface;
+
+class BioAuthV2Service implements BioAuthV2ServiceInterface {
+    const BIO_AUTH_EXPIRY_TIME = 30;
+
+    private $bioClientRepository;
+    private $bioSessionRepository;
+    private $bioAuthSessionRepository;
+
+    public function __construct(
+        BioClientRepositoryInterface $bioClientRepository,
+        BioSessionRepositoryInterface $bioSessionRepository,
+        BioAuthSessionRepositoryInterface $bioAuthSessionRepository
+    ) {
+        $this->bioClientRepository = $bioClientRepository;
+        $this->bioSessionRepository = $bioSessionRepository;
+        $this->bioAuthSessionRepository = $bioAuthSessionRepository;
+    }
+
+    /**
+     * @param $ip_address
+     * @param BioAuthV2ControllerInterface $endpoint
+     * @return BioAuthV2ControllerInterface
+     */
+    public function performStage1($ip_address, BioAuthV2ControllerInterface $endpoint)
+    {
+        // TODO
+    }
+
+    public function performStage2(
+        $session_id,
+        $client_id,
+        $client_random,
+        $client_mac,
+        $ip_address,
+        BioAuthV2ControllerInterface $endpoint
+    ) {
+        // TODO: Implement performStage2() method.
+    }
+
+    /**
+     * @param $client_id
+     * @return bool
+     */
+    private function verifyClientIdNotMalformed($client_id) {
+        if (!$client_id) {
+            return false;
+        }
+
+        if (strlen(base64_decode($client_id)) !== 16) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * @param $client_id
+     * @return \BiometricSite\Model\BiometricClient|false
+     */
+    private function verifyClientIdBelongsToValidClient($client_id) {
+        return $this->bioClientRepository->findByClientId($client_id);
+    }
+
+    /**
+     * @return string
+     */
+    private function generateUnusedSessionId() {
+        do {
+            // Generate session_id securely
+            $cryptographicallySecure = false;
+            $session_id = base64_encode(openssl_random_pseudo_bytes(16, $cryptographicallySecure));
+            if ($cryptographicallySecure === false) {
+                throw new \UnexpectedValueException("System is not cryptographically secure", 500);
+            }
+            // See if session_id has already been used for a session
+            $session_idAlreadyExists = $this->bioSessionRepository->findBySessionId($session_id);
+        } while ($session_idAlreadyExists !== false);
+
+        return $session_id;
+    }
+
+}
